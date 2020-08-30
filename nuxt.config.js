5d32b9a6f117475dc2fa4693644394fe16c1c1a5
y.sekiguchi <sekiguchi@ham.works>
2020-08-30 14:58:23 +0900

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 const axios = require('axios')
-require('dotenv').config()
 const { API_KEY, API_URL } = process.env
 
 export default {
--- pages/information/detail.vue
+++ pages/information/detail.vue
@@ -8,14 +8,3 @@
     <div class="w-full md:max-w-3xl mx-auto pt-20">aaaa sssss</div>
   </layout-wrapper>
 </template>
-
-<script>
-import LayoutWrapper from '~/components/LayoutWrapper'
-import LayoutVisual from '~/components/LayoutVisual'
-export default {
-  components: {
-    LayoutWrapper,
-    LayoutVisual,
-  },
-}
-</script>
