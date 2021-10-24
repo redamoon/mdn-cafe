efbaca20ad2220278f6ab76b9676d595d84980a6
redamoon <yuuki60824@gmail.com>
2021-10-25 02:25:15 +0900

    update Nuxt 3

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -2,11 +2,29 @@
   <header class="w-full absolute md:static bg-black px-2 py-2 z-50">
     <div class="md:max-w-3xl mx-auto md:flex md:items-center">
       <div
-        class="w-full md:flex mx-auto px-6 md:px-0 flex justify-between items-center h-16"
+        class="
+          w-full
+          md:flex
+          mx-auto
+          px-6
+          md:px-0
+          flex
+          justify-between
+          items-center
+          h-16
+        "
       >
         <h1>
           <a
-            class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap"
+            class="
+              text-white text-sm
+              font-bold
+              leading-relaxed
+              inline-block
+              mr-4
+              py-2
+              whitespace-no-wrap
+            "
             href="/"
           >
             MdN Cafe
@@ -28,35 +46,80 @@
         </div>
       </div>
       <nav
-        class="w-full md:block absolute left-0 md:static bg-black md:bg-none z-20"
+        class="
+          w-full
+          md:block
+          absolute
+          left-0
+          md:static
+          bg-black
+          md:bg-none
+          z-20
+        "
         :class="isOpen ? 'block' : 'hidden'"
       >
         <ul class="md:flex md:justify-end md:items-end">
           <li class="w-full md:w-auto md:ml-5">
             <a
               href="/concept/"
-              class="text-white md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+              class="
+                text-white
+                md:block
+                inline-block
+                md:py-0
+                py-5
+                px-5
+                md:px-0
+                w-full
+              "
               >コンセプト</a
             >
           </li>
           <li class="w-full md:w-auto md:ml-5">
             <a
               href="/shop/"
-              class="text-white md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+              class="
+                text-white
+                md:block
+                inline-block
+                md:py-0
+                py-5
+                px-5
+                md:px-0
+                w-full
+              "
               >店舗情報</a
             >
           </li>
           <li class="w-full md:w-auto md:ml-5">
             <a
               href="/menu/"
-              class="text-white md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+              class="
+                text-white
+                md:block
+                inline-block
+                md:py-0
+                py-5
+                px-5
+                md:px-0
+                w-full
+              "
               >メニュー</a
             >
           </li>
           <li class="w-full md:w-auto md:ml-5">
             <a
               href="/information/"
-              class="text-white md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+              class="
+                text-white
+                md:block
+                inline-block
+                md:py-0
+                py-5
+                px-5
+                md:px-0
+                w-full
+              "
               >お知らせ</a
             >
           </li>
